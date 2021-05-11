6db971cdf2573f5ffb2939bb7540f89a3157f806
osboxes.org <osboxes@node2.attlocal.net>
2021-05-11 17:35:47 -0400

    Changes to site name

--- webapp/src/main/webapp/index.php
+++ webapp/src/main/webapp/index.php
@@ -9,7 +9,7 @@
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-<title>Data Service Group</title>
+<title>Nde Service Group</title>
 <!-- Meta tag Keywords -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
